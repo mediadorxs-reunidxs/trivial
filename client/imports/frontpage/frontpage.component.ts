df96c0174e3e4532880c13f33455713db8eadd92
Antonio Tapiador del Dujo <atd@singularities.org>
2017-02-21 12:47:39 +0100

    Frontpage scaffold

--- client/imports/frontpage/frontpage.component.ts
+++ client/imports/frontpage/frontpage.component.ts
@@ -3,7 +3,73 @@ import { Component } from '@angular/core';
 import template from "./frontpage.html";
 
 @Component({
+  selector: 'frontpage',
   template
 })
 export class FrontpageComponent {
+  products = [
+    {
+      title: 'Básico',
+      prize: 50,
+      box: 'básica',
+      board: 'estándar',
+      questions: 200,
+      participants: 10
+    },
+    {
+      title: 'Standard',
+      prize: 70,
+      box: 'básica',
+      board: 'estándar',
+      questions: 300,
+      participants: 15
+    },
+    {
+      title: 'Avanzado',
+      prize: 90,
+      box: 'total',
+      board: 'personalizado',
+      questions: 300,
+      participants: 15
+    },
+    {
+      title: 'Super Pro',
+      prize: 110,
+      box: 'total',
+      board: 'personalizado',
+      questions: "ilimitadas",
+      participants: "ilimitadas"
+    }
+  ];
+
+  features = [
+    {
+      text: 'Trivial con vuestras preguntas',
+      value: true
+    },
+    {
+      text: 'Tarjetas personalizadas',
+      value: true
+    },
+    {
+      text: 'Caja personalizada',
+      value: 'box'
+    },
+    {
+      text: 'Tablero',
+      value: 'board'
+    },
+    {
+      text: 'Fichas',
+      value: true
+    },
+    {
+      text: 'Preguntas',
+      value: 'questions'
+    },
+    {
+      text: 'Participantes',
+      value: 'participants'
+    }
+  ]
 }
