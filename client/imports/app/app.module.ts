e61ebd7dfe077317b7387993ee2b97c5d944f352
Antonio Tapiador del Dujo <atd@singularities.org>
2017-02-21 11:27:11 +0100

    FlexLayout module

--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { Angulartics2Module, Angulartics2GoogleAnalytics } from 'angulartics2';
 
 import { Trivial } from './app.component';
@@ -15,6 +16,7 @@ const ROUTES: Routes = [
   imports: [
     BrowserModule,
     MaterialModule,
+    FlexLayoutModule,
     RouterModule.forRoot(ROUTES),
     Angulartics2Module.forRoot([ Angulartics2GoogleAnalytics ])
   ],
